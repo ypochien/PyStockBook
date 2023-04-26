5c7302ac8d6969d1e87fbd729e0773a4cc95d23d
Po Chien Yang <ypochien@users.noreply.github.com>
2023-04-26 14:21:04 +0800

    feat: cli

--- PyStockBook/cli/__init__.py
+++ PyStockBook/cli/__init__.py
@@ -1,2 +1,14 @@
+from loguru import logger
+
+#
+# from PyStockBook.stock import Stock
+from PyStockBook.book import Book
+
+
 def run():
-    pass
+    v = Book()
+    v.update_stock_close_price()
+
+
+def main():
+    run()
